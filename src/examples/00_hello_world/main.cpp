6b61f2aed8bcfa3c9e25f6f057e9b3814d948a8d
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-01-24 00:39:21 +0000

    anatomy of C++ program hello world

--- src/examples/00_hello_world/main.cpp
+++ src/examples/00_hello_world/main.cpp
@@ -1,7 +1,14 @@
-#include<iostream>
+#include<iostream>  //i'm going to use something/function object from iostream
 
-int main()
+using std::cout;
+
+/*
+multi line comment
+comments
+*/
+
+int main() // the entry point to a C++ program; every C++ requires a function named main
 {
-    std::cout<<"Hello World!";
+    cout<<"Hello World!";  //cout is in iostream
     return 0;
 }
