3c5bc66bb74a167931cd1279964b70ecf8646703
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-01-24 00:54:43 +0000

    code, test, and main program w int test case

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,11 @@
+#include "int.h"
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    cout<<"Echo: "<<echo_variable(10)<<"\n";
 
     return 0;
 }
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,9 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "int.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Verify Test echo variable", "verification") {
+	REQUIRE(echo_variable(10) == 10);
+}
+
 
 /*test case echo_variable */
 
