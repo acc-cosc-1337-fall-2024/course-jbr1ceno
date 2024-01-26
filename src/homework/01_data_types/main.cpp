12309bdc997abab69d51cebb8b98614b2faa31e1
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-01-26 01:05:05 +0000

    HW1 Solution

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,27 @@
-﻿//write include statements
+﻿#include <iostream>
 
+//write include statements
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+	int num = 0;
+
+	cout<<"Enter a number: ";
+	cin>>num; //value from keyboard saved into the num variable
+
+	int result = 0;
+	result = multiply_numbers(num);
+
+	cout<<"Value is: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Value is: "<<result<<"\n";
 
 	return 0;
 }
