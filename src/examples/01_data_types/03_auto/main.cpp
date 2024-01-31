b190e4989263148b6bb2de9e94d39eac1f723ef4
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-01-31 00:18:25 +0000

    using the auto keyword

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,17 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 10;
+    cout<<"Integer: "<<num<<"\n";
+
+    auto num1 = 10.5;
+    cout<<"Double: "<<num1<<"\n";
+    
+    auto char1 = 'a';
+    cout<<"Char: "<<char1<<"\n";
 
     return 0;
 }
