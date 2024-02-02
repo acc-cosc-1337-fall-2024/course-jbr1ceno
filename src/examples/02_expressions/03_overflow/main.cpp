2f12abfbfca3f141d95192f81c9ab80eaf86db6e
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-02 00:31:09 +0000

    overflow

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,18 @@
+#include<climits>
+#include<iostream>
+
+using std::cout;
 
 int main()
 {
+    //Overflow example
+    auto max_int = INT_MAX;
+    cout<<"Max int: "<<max_int<<"\n";
+
+    max_int = max_int + 1;  //overflow
+    cout<<"Max int: "<<max_int<<"\n";
+
+
 
     return 0;
 }
