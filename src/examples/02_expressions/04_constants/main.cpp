99b649cc0782c5c6391aef23e7cfe3b42e188690
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-02 01:11:58 +0000

    calculating area with constants

--- src/examples/02_expressions/04_constants/main.cpp
+++ src/examples/02_expressions/04_constants/main.cpp
@@ -1,5 +1,17 @@
+#include<iostream>
+#include "constants.h"
+
+using std::cin; using std::cout;
 
 int main()
 {
+    auto radius = 0;
+    cout<<"Enter radius: ";
+    cin>>radius;
+
+    auto area = get_area_of_circle(radius);
+
+    cout<<"Area: "<<area<<"\n";
+
     return 0;
 }
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "casting.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -15,7 +16,12 @@ TEST_CASE("Test implicit data type casting double to int", "verify double to int
 	REQUIRE(convert_double_to_int(10.99) == 10);
 }
 
-TEST_CASE("Test explicit data type casting", "verify double to int conversion") {
+TEST_CASE("Test explicit data type casting") {
 	
 	REQUIRE(static_cast_double_int(10.99) == 10);
+}
+
+TEST_CASE("Test constant values-get area of circle") {
+	
+	REQUIRE(get_area_of_circle(10.0) == 314.159); //Call function then have parameter == to answer
 }
