91ef5b80b6fff8941be378df940f770c687da2d2
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-07 00:53:42 +0000

    is vowel example

--- src/examples/03_conditionals/01_if/main.cpp
+++ src/examples/03_conditionals/01_if/main.cpp
@@ -6,27 +6,15 @@ using std::cin;
 
 int main() 
 {
-	//create a bool variable named even
-	//create an int variable named value
-	auto even = false;
-	int value = 0;
+	auto result = false;
+	auto ch = 'a';
 
-	//prompt user for a keyboard number
-	//assign the number to the value variable
-	cout<<"Enter a number: ";
-	cin>>value;
+	cout<<"Enter a letter: ";
+	cin>>ch;
 
-	//call the is_even function with value as its argument
-	//and assign its return value to the even variable
-	even = is_even(value);
+	result = is_vowel(ch);
 
-
-	//display even if number is even
-	cout<<even<<"\n";
-
-	auto odd = is_odd(value);
-	cout<<odd<<"\n";
-
+	cout<<result<<"\n";
 
 	return 0;
 }
