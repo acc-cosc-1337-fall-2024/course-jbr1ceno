7f4e49b4c5a979ec9cb5a4471f0694f4ba089e21
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-09 00:43:55 +0000

    switch example

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cin; using std::cout; using std::string;
 
 int main() 
 {
-	
+	auto option = 0;
+	string result = "";
+
+	cout<<"Enter an option: ";
+	cin>>option;
+
+	result = menu(option);
+
+	cout<<result<<"\n";
 
 	return 0;
 }
