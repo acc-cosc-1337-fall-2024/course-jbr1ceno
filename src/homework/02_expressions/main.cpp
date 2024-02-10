c8968ed1f51b8f9d8d37c0a0ec32f72e429c0bee
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-10 02:20:19 +0000

    COSC 1337 Homework 2 Restaurant Bill

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,41 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include <iostream>
+#include <cmath> //using cmath to round the amounts to two decimal points
 
 //write namespace using statement for cout
+using std::cin; using std::cout;
+
 
 int main()
 {
+	//testing rounding to two decimal points
+	/*double num = 3.647;
+
+	double roundnum = round(num*100) / 100;
+
+	cout<<" The number "<<num<<" rounded to two decimal points is: "<<roundnum;*/
+
+	double meal_amount= 0;
+	double tip_rate = 0;
+	double tip_amount = 0;
+	double tax_amount = 0;
+
+	cout<<"Enter a meal amount: $";
+	cin>>meal_amount;
 	
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Enter tip rate (no percent symbol): ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	double total_cost = meal_amount + tip_amount + tax_amount;
+
+	cout<<"\n"<<"RESTAURANT BILL"<<"\n";
+	cout<<"\n"<<"Meal Amount: $"<<meal_amount<<"\n"<<"Sales Tax: $"<<round(tax_amount*100) / 100
+	<<"\n"<<"Tip Amount: $"<<round(tip_amount*100) / 100<<"\n"<<"Total: $"<<round(total_cost*100) / 100<<"\n";
 
 	return 0;
 }
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -6,9 +6,20 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify sum_numbers function") 
+/*TEST_CASE("Verify sum_numbers function") 
 {
 	REQUIRE(add_numbers(5, 5) == 10);
 	REQUIRE(add_numbers(10, 10) == 20);
+}*/
+
+TEST_CASE("Verify the get sales tax amount function")
+{
+	REQUIRE(get_sales_tax_amount(10) == 0.675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
 }
 
+TEST_CASE("Verify the get tip amount function")
+{
+	REQUIRE(get_tip_amount(20, 15) == 3.0);
+	REQUIRE(get_tip_amount(20, 20) == 4.0);
+}
