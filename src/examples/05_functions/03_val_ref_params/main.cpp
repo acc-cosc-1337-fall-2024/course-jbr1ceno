03a1625ef8d43a206163e81c2c46ebeca245be47
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-23 00:26:37 +0000

    intro to refence  variable

--- src/examples/05_functions/03_val_ref_params/main.cpp
+++ src/examples/05_functions/03_val_ref_params/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+using std::cout;
+
 int main()
 {
+    int num = 5;
+    int& num_ref = num; //reference variable-reference the address of another variable
+    int num1 = 10;
+    int& num1_ref = num1;
+
+    cout<<"Memory address of num: "<<&num_ref<<"\n";
+    cout<<"Memory address of num1: "<<&num1_ref<<"\n\n";
+
+    cout<<"Show me the value of the variable num that you reference: "<<num_ref<<"\n";
+    num_ref = 10;
+    cout<<"Show me the value of the variable num that you reference: "<<num_ref<<"\n";
+
     return 0;
 }
