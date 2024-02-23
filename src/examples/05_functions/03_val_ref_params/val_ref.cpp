32ba108adeb56cc339ec5405a913db820d5ba3fa
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-02-23 00:38:53 +0000

    function const reference params

--- src/examples/05_functions/03_val_ref_params/val_ref.cpp
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -9,3 +9,8 @@ void reference_params(int& num)
 {
     num = 10;
 }
+
+void const_reference_param(const int& num)
+{
+    //num = 90; statement shows that we can protect variable data by using const which means read only
+}
--- src/examples/05_functions/03_val_ref_params/val_ref.h
+++ src/examples/05_functions/03_val_ref_params/val_ref.h
@@ -1,2 +1,3 @@
 void value_params(int num);
 void reference_params(int& num);
+void const_reference_param(const int& num); //protect data with const-read only
