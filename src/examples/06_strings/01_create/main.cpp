b58d3ab7b0e3a7908099531c764b36163464b7a4
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-03-01 23:53:44 +0000

    string size and capacity

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -8,7 +8,21 @@ int main()
     string lang = "C++";
 
     cout<<lang<<"\n";
-    cout<<"Size of string: "<<lang.size()<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //max number of strings, the lang variable can hold
+
+    lang.append("123456789012");
+
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //max number of strings, the lang variable can hold
+
+    lang.push_back('3');
+
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //max number of strings, the lang variable can hold
+
 
     return 0;
 }
