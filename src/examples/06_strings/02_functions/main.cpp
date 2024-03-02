b47e3cb7e5a354cf5f0c8bb4534af92f2b67284d
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-03-02 02:46:50 +0000

    create strings

--- src/examples/06_strings/02_functions/main.cpp
+++ src/examples/06_strings/02_functions/main.cpp
@@ -4,11 +4,20 @@ using std::string;
 
 int main()
 {
-    string lang = "C++";
+    string s0 = "C++ is cool";
+    cout<<s0<<"\n";
 
-    //display_str_for(lang);
-    //display_for_ref(lang);
-    display_for_range(lang);
+    string s1(s0, 4, 7); //takes s0, starts from size number 4 and the capacity is 7
+    cout<<s1<<"\n";
+
+    string s2("another character sequence", 12);
+    cout<<s2<<"\n";
+
+    string s3(10, 'x');
+    cout<<s3<<"\n";
+
+    string s4(10, 42); //10*
+    cout<<s4<<"\n";
 
     return 0;
 }
