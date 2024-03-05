d978334e6146558b456a8642009f59db03103d7b
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-03-05 23:52:42 +0000

    create and iterate a vector

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using std::cout; using std::vector;
+
 int main()
 {
+    vector<int> nums {7, 2, 9}; //create and initialize a vector
+    cout<<nums[1]<<"\n";
+
+    for(auto i = 0; i < nums.size(); i++) //iterate a vector with for loop
+    {
+        cout<<nums[i]<<"\n";
+    }
+
+    cout<<"\n";
+
+    for(auto num: nums) //iterate a vector with for range
+    {
+        cout<<num<<"\n";
+    }
+
     return 0;
 }
