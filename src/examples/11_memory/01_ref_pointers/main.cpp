eb8de3a6279ac249c9b1cb2476304fa5bb8ce5bc
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-04-05 03:29:28 +0000

    reference review, intro to pointer

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -1,8 +1,27 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	int num1 = 5;
+	int& num1_ref = num1;
+
+	cout<<num1_ref<<"\n";
+	num1_ref = 10;
+
+	cout<<num1_ref<<"\n";
+
+	cout<<num1<<"\n";
+
+	int num2 = 5; //create variable num2
+	int* num2_ptr = &num2; //&num2 tell the pointer to give me the memory address of num2 and save it to the num2_ptr (pointer)
+	cout<<*num2_ptr<<"\n";
+
+	*num2_ptr = 10; // with *num2_ptr; whatever you're pointing to, I want to work with/change that value
+	cout<<*num2_ptr<<"\n";
+	cout<<num2<<"\n";
 	
 	return 0;
 }
