0abdab65b0a2942f864562800a6672f13198412c
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-04-05 04:18:55 +0000

    allocate memory with new and pointer

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -5,15 +5,15 @@ using std::cout;
 
 int main() 
 {
-	int num1 = 5, num2 = 10;
-	int* num_ptr; //don't have to point to anything
-	num_ptr = &num1; //no asterisk *, meaning not gonna work with value but gonna work with address
-	cout<<"Address of num1: "<<&num1<<"\n";
-	cout<<"Address that num_ptr points to: "<<num_ptr<<"\n"; 	//num_ptr saving address of num1 to its memory block
-	cout<<"Value of the variable num_ptr points to: "<<*num_ptr<<"\n";	//with asterisk * work with values, *num_ptr gors to memory block
+	//This is to show memory, NOT RECOMMENDED way of programming for beginners or expert programmers
+	//create memory
+	int* num = new int(5); //create memory on the heap
 
-	num_ptr = &num2;
-	cout<<*num_ptr<<"\n"; //with asterisk * work with values
+	//use memory
+	cout<<*num<<"\n";
+
+	//delete/free memory
+	delete num;
 
 	
 	return 0;
