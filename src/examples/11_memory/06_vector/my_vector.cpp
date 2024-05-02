dc872f0481969f9c57a862f950ac9d13f36e0a6c
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-05-02 22:37:32 +0000

    My vector constructor destructor

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,15 @@
 #include "my_vector.h"
 
+using std::cout;
 //
+Vector::Vector(int s) : capacity(s), elements{new int[s]}
+{
+    cout<<"Memory allocated at: "<<elements<<"\n";
 
+}
+
+Vector::~Vector()
+{
+    delete[] elements;
+    cout<<"Deleted memory at: "<<elements<<"\n";
+}
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,18 @@
 //
+#include<iostream>
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+class Vector
+{
+public:
+    Vector(int size); //constructor
+    ~Vector();        //destructor
+private:
+    int size{0}; //keep track of the current index and the number of elements in the list
+    int capacity; //the max number of elements the list can store
+    int* elements; //list of elements in this vector
+};
+
+#endif
